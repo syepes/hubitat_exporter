5a61ec9da8cacdfdfce3f656b6c55a38570fc9a9
Sebastian YEPES <syepes@gmail.com>
2021-03-23 21:42:06 +0100

    InitVersion

---
+++ src/hub.rs
@@ -0,0 +1,351 @@
+use reqwest::blocking::Client;
+
+use serde::{de, Deserialize, Deserializer};
+use serde_json::Value;
+
+#[derive(Debug, Default)]
+pub struct HubInfo<'a> {
+  pub ip:               Option<&'a str>,
+  pub auth_usr:         Option<&'a str>,
+  pub auth_pwd:         Option<&'a str>,
+  pub api_id:           Option<&'a str>,
+  pub api_access_token: Option<&'a str>,
+  pub client:           Option<Client>,
+}
+
+fn de_strings<'de, D: Deserializer<'de>>(deserializer: D) -> Result<String, D::Error> {
+  Ok(match Value::deserialize(deserializer)? {
+    Value::String(s) => s,
+    Value::Number(num) => num.to_string(),
+    Value::Bool(b) => b.to_string(),
+    Value::Null => "".to_string(),
+    _ => return Err(de::Error::custom("wrong type")),
+  })
+}
+
+#[derive(Debug, Deserialize, Default, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct DeviceInventory {
+  pub location_name:      String,
+  pub hub_name:           String,
+  #[serde(deserialize_with = "de_strings")]
+  pub is_component:       String,
+  #[serde(deserialize_with = "de_strings")]
+  pub zigbee_id:          String,
+  pub device_type_name:   String,
+  #[serde(deserialize_with = "de_strings")]
+  pub location_id:        String,
+  #[serde(deserialize_with = "de_strings")]
+  pub id:                 String,
+  #[serde(deserialize_with = "de_strings")]
+  pub lan_id:             String,
+  #[serde(deserialize_with = "de_strings")]
+  pub hub_id:             String,
+  #[serde(deserialize_with = "de_strings")]
+  pub display_as_child:   String,
+  #[serde(deserialize_with = "de_strings")]
+  pub mesh_enabled:       String,
+  pub name:               String,
+  pub r#type:             String,
+  #[serde(deserialize_with = "de_strings")]
+  pub disabled:           String,
+  pub status:             String,
+  #[serde(deserialize_with = "de_strings")]
+  pub linked_device:      String,
+  #[serde(deserialize_with = "de_strings")]
+  pub label:              String,
+  #[serde(deserialize_with = "de_strings")]
+  pub device_network_id:  String,
+  #[serde(deserialize_with = "de_strings")]
+  pub device_type_id:     String,
+  #[serde(deserialize_with = "de_strings")]
+  pub last_activity_time: String,
+  #[serde(deserialize_with = "de_strings")]
+  pub parent_device_id:   String,
+  #[serde(deserialize_with = "de_strings")]
+  pub display_name:       String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DeviceIDs {
+  pub id: String,
+  name:   String,
+  label:  String,
+}
+
+#[derive(Debug, Deserialize, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct Device {
+  pub id:           String,
+  pub name:         String,
+  pub label:        String,
+  pub r#type:       String,
+  pub attributes:   Vec<DeviceAttribute>,
+  pub capabilities: Vec<serde_json::Value>,
+  pub commands:     Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DeviceAttribute {
+  pub name:          String,
+  #[serde(deserialize_with = "de_strings")]
+  pub current_value: String,
+  pub data_type:     String,
+  #[serde(default)]
+  pub values:        Vec<String>,
+}
+
+impl DeviceAttribute {
+  pub fn get_numeric_value(&self) -> Option<String> {
+    match self {
+      Self { ref name, current_value, .. } if name == "acceleration" => {
+        if current_value == "active" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "alarm" => {
+        if current_value == "off" || current_value == "inactive" {
+          Some("0".to_string())
+        } else {
+          Some("1".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "presence" => {
+        if current_value == "present" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "presence" => {
+        if current_value == "present" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "switch" => {
+        if current_value == "on" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "button" => {
+        if current_value == "pushed" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "carbonMonoxide" => {
+        if current_value == "detected" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "status" => {
+        if current_value == "playing" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "consumableStatus" => {
+        if current_value == "good" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "contact" => {
+        if current_value == "closed" {
+          Some("0".to_string())
+        } else {
+          Some("1".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "indicatorStatus" => {
+        if current_value == "when on" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "lock" => {
+        if current_value == "locked" {
+          Some("0".to_string())
+        } else {
+          Some("1".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "motion" => {
+        if current_value == "active" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "shock" => {
+        if current_value == "detected" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "mute" => {
+        if current_value == "muted" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "sleeping" => {
+        if current_value == "sleeping" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "smoke" => {
+        if current_value == "detected" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "sound" => {
+        if current_value == "detected" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "tamper" => {
+        if current_value == "detected" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "thermostatMode" => {
+        if current_value == "off" {
+          Some("0".to_string())
+        } else {
+          Some("1".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "thermostatFanMode" => {
+        if current_value == "off" {
+          Some("0".to_string())
+        } else {
+          Some("1".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "thermostatOperatingState" => {
+        if current_value == "heating" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "thermostatSetpointMode" => {
+        if current_value == "followSchedule" {
+          Some("0".to_string())
+        } else {
+          Some("1".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "timedSession" => {
+        if current_value == "running" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "touch" => {
+        if current_value == "touched" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "valve" => {
+        if current_value == "open" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "camera" => {
+        if current_value == "on" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "water" => {
+        if current_value == "wet" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "windowShade" => {
+        if current_value == "opening" || current_value == "partially open" || current_value == "open" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "optimisation" => {
+        if current_value == "active" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "windowFunction" => {
+        if current_value == "active" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "rain" => {
+        if current_value == "active" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "rainHeavy" => {
+        if current_value == "active" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref name, current_value, .. } if name == "heatAlarm" => {
+        if current_value == "overheat" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      Self { ref data_type, current_value, .. } if data_type == "NUMBER" => Some(current_value.to_string()),
+      Self { ref current_value, .. } if current_value == "on" => {
+        if current_value == "on" {
+          Some("1".to_string())
+        } else {
+          Some("0".to_string())
+        }
+      },
+      _ => None,
+    }
+  }
+}
---
+++ src/main.rs
@@ -0,0 +1,233 @@
+mod hub;
+
+use std::{collections::HashMap, io::Write};
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+use log::LevelFilter;
+
+extern crate clap_v3;
+use clap_v3::{App, Arg, ArgMatches};
+
+extern crate tiny_http;
+use tiny_http::{Response, Server};
+
+use anyhow::{anyhow, Result};
+use convert_case::{Case, Casing};
+use reqwest::blocking::RequestBuilder;
+
+fn main() {
+  env_logger::Builder::new().format(|buf, record| writeln!(buf, "{} {}:{} [{}] - {}", chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"), record.file().unwrap_or("unknown"), record.line().unwrap_or(0), record.level(), record.args())).filter(None, LevelFilter::Warn).init();
+
+  let app = App::new("").version(env!("CARGO_PKG_VERSION"))
+                        .author(env!("CARGO_PKG_AUTHORS"))
+                        .about(env!("CARGO_PKG_DESCRIPTION"))
+                        .arg(Arg::with_name("listener").long("listener").env("LISTENER").default_value("0.0.0.0:8000"))
+                        .arg(Arg::with_name("he_ip").short('h').long("hubitat_ip").env("HE_IP").required(true))
+                        .arg(Arg::with_name("he_api_id").short('i').long("hubitat_api_id").env("HE_API_ID").required(true))
+                        .arg(Arg::with_name("he_api_token").short('t').long("hubitat_api_access_token").env("HE_API_TOKEN").required(true))
+                        .arg(Arg::with_name("he_dd").short('d').long("hubitat_device_details").env("HE_DD").default_value("true").required(false))
+                        .arg(Arg::with_name("he_auth_usr").short('u').long("hubitat_auth_usr").env("HE_AUTH_USR").required(false))
+                        .arg(Arg::with_name("he_auth_pwd").short('p').long("hubitat_auth_pwd").env("HE_AUTH_PWD").requires("he_auth_usr").required(false))
+                        .get_matches();
+
+  let mut he = hub::HubInfo { ip: app.value_of("he_ip"), auth_usr: None, auth_pwd: None, api_id: app.value_of("he_api_id"), api_access_token: app.value_of("he_api_token"), client: None };
+
+  if let Ok(server) = Server::http(app.value_of("listener").unwrap()) {
+    info!("started on http://{}", app.value_of("listener").unwrap());
+
+    if app.is_present("he_dd") {
+      info!("detailed mode is turned on");
+      get_log(&mut he, &app);
+    }
+
+    for request in server.incoming_requests() {
+      let dev_inv = get_device_inventory(&mut he, &app);
+      trace!("dev_inv:{:#?}", dev_inv);
+
+      let devs = get_device_details(&he, get_device_ids(&he));
+      trace!("devs:{:#?}", devs);
+
+      let m = build_metrics(devs, &dev_inv);
+      let response = Response::from_string(m);
+      let _ = request.respond(response);
+    }
+  } else {
+    error!("Starting web server with listener {:?}", app.value_of("listener"));
+  }
+}
+
+fn build_metrics(devs: Result<Vec<hub::Device>, anyhow::Error>, dev_inv: &Option<HashMap<String, hub::DeviceInventory>>) -> String {
+  let mut metrics: String = "".to_owned();
+
+  if let Ok(dev_details) = devs {
+    for i in dev_details.iter() {
+      if let Some(d) = dev_inv {
+        match d.get(&i.id) {
+          Some(d) => {
+            // Detailed mode without the Device Inventory
+            for a in i.attributes.iter() {
+              if let Some(v) = a.get_numeric_value() {
+                let m = &format!("{metric}{{hub_name=\"{hub_name}\",hub_location_name=\"{hub_location_name}\",device_network_id=\"{device_network_id}\",device_driver_type=\"{device_driver_type}\",device_driver=\"{device_driver}\",device_name=\"{device_name}\",device_label=\"{device_label}\"}} {val}\n",
+                                 metric = a.name.to_case(Case::Snake),
+                                 hub_name = d.hub_name,
+                                 hub_location_name = d.location_name,
+                                 device_network_id = d.device_network_id,
+                                 device_driver_type = i.r#type,
+                                 device_driver = d.device_type_name,
+                                 device_name = i.name,
+                                 device_label = i.label,
+                                 val = v);
+                metrics.push_str(m);
+              }
+            }
+          },
+          _ => {
+            if !&i.id.is_empty() {
+              warn!("Device ID: {:?} not found", &i.id);
+            }
+          },
+        }
+      } else {
+        // Simple mode without the Device Inventory
+        for a in i.attributes.iter() {
+          if let Some(v) = a.get_numeric_value() {
+            let m = &format!("{metric}{{device_name=\"{device_name}\",device_label=\"{device_label}\",device_driver_type=\"{device_driver_type}\"}} {val}\n", metric = a.name.to_case(Case::Snake), device_name = i.name, device_label = i.label, device_driver_type = i.r#type, val = v);
+            metrics.push_str(m);
+          }
+        }
+      }
+    }
+  }
+
+  metrics
+}
+
+fn get_log(he: &mut hub::HubInfo, app: &ArgMatches) {
+  if let Ok(c) = reqwest::blocking::Client::builder().user_agent(env!("CARGO_PKG_NAME")).cookie_store(true).danger_accept_invalid_certs(true).connection_verbose(true).build() {
+    let req_url = format!("http://{he_ip}/login", he_ip = he.ip.unwrap());
+
+    let req: RequestBuilder;
+    if app.is_present("he_auth_usr") && app.is_present("he_auth_pwd") {
+      debug!("Auth on {:?}/{:?}", app.value_of("he_auth_usr"), app.value_of("he_auth_pwd"));
+
+      let mut params = HashMap::new();
+      params.insert("username", app.value_of("he_auth_usr").unwrap().to_string());
+      params.insert("password", app.value_of("he_auth_pwd").unwrap().to_string());
+      req = c.post(req_url).form(&params);
+    } else {
+      req = c.post(req_url);
+    }
+
+    match req.send() {
+      Ok(r) => {
+        debug!("resp:{:#?}", r);
+
+        if r.status().is_success() {
+          he.client = Some(c)
+        } else {
+          he.client = None;
+          error!("request post failed: {:?}", r);
+        }
+      },
+      Err(e) => {
+        he.client = None;
+        error!("request post failed: {:?}", e);
+      },
+    }
+  }
+}
+
+fn get_device_inventory(he: &mut hub::HubInfo, app: &ArgMatches) -> Option<HashMap<String, hub::DeviceInventory>> {
+  if let Some(c) = &he.client {
+    let req_url = format!("http://{he_ip}/device/list/all/data", he_ip = he.ip.unwrap());
+
+    match c.get(req_url).send() {
+      Ok(r) => {
+        if r.status().is_success() {
+          debug!("resp:{:#?}", r);
+          if let Some(_h) = r.headers().get(reqwest::header::X_FRAME_OPTIONS) {
+            get_log(he, app);
+            error!("loging failed");
+            return None;
+          }
+          match r.json::<Vec<hub::DeviceInventory>>() {
+            Ok(dev) => {
+              let inv = dev.into_iter().map(|item| (item.id.clone(), item)).collect();
+              Some(inv)
+            },
+            Err(e) => {
+              error!("json parsing failed: {:?}", e);
+              None
+            },
+          }
+        } else {
+          error!("request get failed: {:?}", r);
+          None
+        }
+      },
+      Err(e) => {
+        error!("request get failed: {:?}", e);
+        None
+      },
+    }
+  } else {
+    None
+  }
+}
+
+fn get_device_ids(he: &hub::HubInfo) -> Result<Vec<u32>, anyhow::Error> {
+  let req_url = format!("http://{he_ip}/apps/api/{he_api_id}/devices?access_token={he_api_token}", he_ip = he.ip.unwrap(), he_api_id = he.api_id.unwrap(), he_api_token = he.api_access_token.unwrap());
+  let client = reqwest::blocking::Client::builder().user_agent(env!("CARGO_PKG_NAME")).danger_accept_invalid_certs(true).connection_verbose(true).build().expect("Error building client");
+
+  match client.get(req_url).send() {
+    Ok(r) => {
+      debug!("resp:{:#?}", r);
+      if r.status().is_success() {
+        match r.json::<Vec<hub::DeviceIDs>>() {
+          Ok(d) => {
+            let ids: Vec<u32> = d.iter().map(|i| i.id.trim().parse::<u32>()).filter_map(Result::ok).collect();
+            Ok(ids)
+          },
+          Err(e) => Err(anyhow!("json parsing failed: {:?}", e)),
+        }
+      } else {
+        Err(anyhow!("request get failed: {:?}", r))
+      }
+    },
+    Err(e) => Err(anyhow!("request get failed: {:?}", e)),
+  }
+}
+
+fn get_device_details(he: &hub::HubInfo, ids: Result<Vec<u32>, anyhow::Error>) -> Result<Vec<hub::Device>, anyhow::Error> {
+  let mut devs: Vec<hub::Device> = vec![hub::Device::default()];
+
+  if let Ok(dev_ids) = ids {
+    for i in dev_ids.iter() {
+      let req_url = format!("http://{he_ip}/apps/api/{he_api_id}/devices/{dev_id}?access_token={he_api_token}", he_ip = he.ip.unwrap(), he_api_id = he.api_id.unwrap(), he_api_token = he.api_access_token.unwrap(), dev_id = i);
+      let client = reqwest::blocking::Client::builder().user_agent(env!("CARGO_PKG_NAME")).danger_accept_invalid_certs(true).connection_verbose(true).build().expect("Error building client");
+
+      match client.get(req_url).send() {
+        Ok(r) => {
+          debug!("resp:{:#?}", r);
+          if r.status().is_success() {
+            match r.json::<hub::Device>() {
+              Ok(d) => devs.push(d),
+              Err(e) => {
+                error!("json parsing failed: {:?}", e);
+              },
+            }
+          } else {
+            return Err(anyhow!("request get failed: {:?}", r));
+          }
+        },
+        Err(e) => {
+          return Err(anyhow!("request get failed: {:?}", e));
+        },
+      }
+    }
+  }
+
+  Ok(devs)
+}
