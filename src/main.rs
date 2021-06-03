582cbf9d7d94610df8b9f5357ae3b7e2a3b83bb0
Sebastian YEPES <syepes@gmail.com>
2021-06-03 13:36:59 +0200

    Fix logging

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::{collections::HashMap, io::Write};
 #[macro_use]
 extern crate log;
 extern crate env_logger;
-use log::LevelFilter;
 
 extern crate clap_v3;
 use clap_v3::{App, Arg, ArgMatches};
@@ -28,16 +27,17 @@ fn main() {
                         .arg(Arg::with_name("he_dd").short('d').long("hubitat_device_details").env("HE_DD").default_value("true").required(false))
                         .arg(Arg::with_name("he_auth_usr").short('u').long("hubitat_auth_usr").env("HE_AUTH_USR").required(false))
                         .arg(Arg::with_name("he_auth_pwd").short('p').long("hubitat_auth_pwd").env("HE_AUTH_PWD").requires("he_auth_usr").required(false))
-                        .arg(Arg::with_name("v").short('v').multiple(true).required(false))
+                        .arg(Arg::with_name("v").short('v').multiple(true).takes_value(false).required(false))
                         .get_matches();
 
   match app.occurrences_of("v") {
-    0 | 1 => std::env::set_var("RUST_LOG", "error"),
-    2 => std::env::set_var("RUST_LOG", "error"),
-    _ => std::env::set_var("RUST_LOG", "trace"),
+    0 => std::env::set_var("RUST_LOG", "error"),
+    1 => std::env::set_var("RUST_LOG", "info"),
+    2 => std::env::set_var("RUST_LOG", "debug"),
+    3 | _ => std::env::set_var("RUST_LOG", "trace"),
   }
 
-  env_logger::Builder::new().format(|buf, record| writeln!(buf, "{} {}:{} [{}] - {}", chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"), record.file().unwrap_or("unknown"), record.line().unwrap_or(0), record.level(), record.args())).filter(None, LevelFilter::Warn).init();
+  env_logger::Builder::from_default_env().format(|buf, record| writeln!(buf, "{} {} {}:{} [{}] - {}", chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"), record.module_path().unwrap_or("unknown"), record.file().unwrap_or("unknown"), record.line().unwrap_or(0), record.level(), record.args())).init();
 
   let mut he = hub::HubInfo { ip: app.value_of("he_ip"), auth_usr: None, auth_pwd: None, api_id: app.value_of("he_api_id"), api_access_token: app.value_of("he_api_token"), client: None };
 
